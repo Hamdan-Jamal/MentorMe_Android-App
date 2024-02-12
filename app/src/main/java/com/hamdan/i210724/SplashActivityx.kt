279e42114b39a93a7d862b67a54505c1f3837a8f
Hamdan Jamal <hamdanze1@gmail.com>
2024-02-12 15:21:46 +0500

    Initial commit

---
+++ app/src/main/java/com/hamdan/i210724/SplashActivityx.kt
@@ -0,0 +1,18 @@
+package com.hamdan.i210724
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivityx : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_activityx)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivityx, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
