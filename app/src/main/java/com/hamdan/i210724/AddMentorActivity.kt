1280884f61317357fa50753cadd781820ea18ba4
Hamdan Jamal <hamdanze1@gmail.com>
2024-02-13 19:49:43 +0500

    Add Mentor Screen

---
+++ app/src/main/java/com/hamdan/i210724/AddMentorActivity.kt
@@ -0,0 +1,11 @@
+package com.hamdan.i210724
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMentorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_mentor)
+    }
+}
