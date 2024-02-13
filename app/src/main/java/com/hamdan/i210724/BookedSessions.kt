77b9dfbeb1ffe63d335b206be6b4f1de0c1869f4
Hamdan Jamal <hamdanze1@gmail.com>
2024-02-13 22:19:07 +0500

    Booked sessions Activity

---
+++ app/src/main/java/com/hamdan/i210724/BookedSessions.kt
@@ -0,0 +1,11 @@
+package com.hamdan.i210724
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookedSessions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booked_sessions)
+    }
+}
