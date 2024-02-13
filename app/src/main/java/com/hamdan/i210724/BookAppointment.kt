ebae3ef04fd04fed7cd1165b13d89b9011629c7d
Hamdan Jamal <hamdanze1@gmail.com>
2024-02-13 22:36:28 +0500

    Sesions booking activity

---
+++ app/src/main/java/com/hamdan/i210724/BookAppointment.kt
@@ -0,0 +1,11 @@
+package com.hamdan.i210724
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookAppointment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_appointment)
+    }
+}
